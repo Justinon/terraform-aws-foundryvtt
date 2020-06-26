dc986f77aa9f952fa69196e5be4720fa81ea97d6
justin.glommen <jglommen@gmail.com>
2020-06-25 20:04:10 -0700

    Removed required terraform version because Im not sure it works inside a module

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 2.68.0"
+      version = ">= 2.68.0"
     }
     template = {
-      version = "~> 2.1"
+      version = ">= 2.1"
     }
   }
-  required_version = "~> 0.13.0" 
 }

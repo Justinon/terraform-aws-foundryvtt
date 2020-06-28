d359a279dced1729c87c172a8081306a67eef00a
justin.glommen <jglommen@gmail.com>
2020-06-27 18:40:39 -0700

    jsut removing all the cruft

--- ecs.tf
+++ ecs.tf
@@ -78,7 +78,6 @@ resource "aws_ecs_service" "foundry_server" {
       desired_count
     ]
   }
-  depends_on = [aws_iam_role_policy_attachment.ecs_service]
 }
 
 resource "aws_ecs_task_definition" "foundry_server" {

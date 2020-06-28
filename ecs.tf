6a778e7e3c148c360aa68943577f1599a4282f01
justin.glommen <jglommen@gmail.com>
2020-06-27 18:21:44 -0700

    truing again

--- ecs.tf
+++ ecs.tf
@@ -49,7 +49,7 @@ resource "aws_ecs_service" "foundry_server" {
   desired_count                     = 1
   enable_ecs_managed_tags           = true
   health_check_grace_period_seconds = 120
-  iam_role                          = aws_iam_service_linked_role.ecs_service.arn
+  iam_role                          = data.aws_iam_role.ecs_service.arn
   launch_type                       = "FARGATE"
   name                              = "foundry-server-${terraform.workspace}"
   propagate_tags                    = "TASK_DEFINITION"
